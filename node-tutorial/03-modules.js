a5392bcaf730dd17f87d0fb8e20fafd7cedf9f0b
Abhishek Sahu <asahu.m1608@gmail.com>
2022-10-07 16:09:35 +0530

    setup

---
+++ node-tutorial/03-modules.js
@@ -0,0 +1,12 @@
+// Modules
+
+const sankalp = 'sankalp';
+const anshul = 'anshul';
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('susan')
+sayHi(anshul)
+sayHi(sankalp)
