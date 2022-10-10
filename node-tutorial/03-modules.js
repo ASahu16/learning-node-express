82dc94c01b546f86d81fbb4ddea6f682b0b2e5ca
Abhishek Sahu <asahu.m1608@gmail.com>
2022-10-10 09:17:05 +0530

    alternate syntax
    Different syntax to export properties from a module

--- node-tutorial/03-modules.js
+++ node-tutorial/03-modules.js
@@ -5,8 +5,9 @@
 
 const names = require('./04-names');
 const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
 // console.log(names);
-
+console.log(data);
 sayHi('susan')
 sayHi(names.anshul)
 sayHi(names.sankalp)
---
+++ node-tutorial/06-alternative-flavor.js
@@ -0,0 +1,11 @@
+// exports on the go, that is, as you write the code export
+module.exports.items = ['item1', 'item2'];
+
+const person = {
+    name : 'bob',
+}
+
+module.exports.singlePerson = person
+
+// ES module syntax
+// export const singlePerson = person
