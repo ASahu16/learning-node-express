d1e1666663d6b6f31550f46b8f8b0f77946b1e1a
Abhishek Sahu <asahu.m1608@gmail.com>
2022-10-17 15:15:54 +0530

    Demo blocking code

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -1,1 +1,21 @@
-console.log("Hello world! this is my first package");
+const http = require('http');
+
+
+const server = http.createServer((request, response)=>{
+
+    if(request.url === '/')
+        response.end('Home Page');
+    else if(request.url === '/about'){
+        //blocking code
+        for(let i = 0; i < 1000; i++)
+            for(let j = 0; j < 1000; j++)
+                console.log(`${i} ${j}`);
+        response.end('About Page');
+    }
+    else
+        response.end('Error Page');
+});
+
+server.listen(5000, ()=>{
+    console.log('Server started on port: 5000');
+})
