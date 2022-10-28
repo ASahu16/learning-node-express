73fbe3a7bf31b31b759bdb58155a301b0e5fa0dc
Abhishek Sahu <asahu.m1608@gmail.com>
2022-10-28 12:40:13 +0530

    Remove unnecessary code

--- express-tutorial/src/11-methods.js
+++ express-tutorial/src/11-methods.js
@@ -9,15 +9,6 @@ app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people });
 });
 
-app.post('/api/people', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res
-            .status(400)
-            .json({ success: false, msg: 'please provide name value' });
-    }
-    res.status(201).json({ success: true, person: name });
-});
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....');
