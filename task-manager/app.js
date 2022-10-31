5b2e841a19299768f608b0ee9b63da5cbf08fcca
Abhishek Sahu <asahu.m1608@gmail.com>
2022-10-31 16:32:02 +0530

    setup server

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Task Manager App');
+});
+
+const port = 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
