9d9dc475c59a4d351626761cd7f5fce3c4e46f65
Abhishek Sahu <39129685+ASahu16@users.noreply.github.com>
2022-11-01 20:33:47 +0530

    setup and connect to database, fix #29 (#31)

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+// load environment variables from a .env file into process.env
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -16,6 +20,14 @@ const port = 5000;
 
 const start = async () => {
   try {
+    // Since we should always spawn the server
+    // once we are connected to the DB
+    // we wait for the connection to establish
+    //
+    // Note: Since we wrap the below code in try-catch
+    // connection is not established,
+    // server will not start, flow will go to the catch block
+    await connectDB(process.env.ATLAS_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
