89b1a5fad22158a504d67563bc92b4092eea9c1f
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-01 12:55:59 +0530

    Setup api skeleton

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -2,4 +2,20 @@ const getAllTasks = (req, res) => {
   res.send('all items from file');
 };
 
-module.exports = { getAllTasks };
+const createTask = (req, res) => {
+  res.send('create Task');
+};
+
+const getTask = (req, res) => {
+  res.send('Get Individual Task');
+};
+
+const updateTask = (req, res) => {
+  res.send('update Task');
+};
+
+const deleteTask = (req, res) => {
+  res.send('delete Task');
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
