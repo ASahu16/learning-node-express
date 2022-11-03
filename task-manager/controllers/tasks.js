518b030845d70b487ac384fb28e7060eebcc0d5f
Abhishek Sahu <39129685+ASahu16@users.noreply.github.com>
2022-11-03 22:58:51 +0530

    Update controller (#36)
    * Update `createTask` controller
    
    Save task data on cloud
    
    * Handle `error` in controller using `try-catch`
    
    * Update `getAllTasks` controller
    
    Fetch(`find()`) all tasks from the cloud
    
    * Update `getTask` controller
    
    Fetch individual task details with matching `id` from the cloud
    
    * Update `deleteTask` controller
    
    Delete individual task details with matching `id` from the cloud
    
    * Update `updateTask` controller
    
    Update task details with matching `id` on the cloud
    
    * Fix typo

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,21 +1,68 @@
-const getAllTasks = (req, res) => {
-  res.send('all items from file');
+const Task = require('../models/task');
+
+const getAllTasks = async (req, res) => {
+  try {
+    // find all documents
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.send('create Task');
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send('Get Individual Task');
+const getTask = async (req, res) => {
+  try {
+    // find single task documents
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(400).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send('update Task');
+const updateTask = async (req, res) => {
+  try {
+    // update document
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete Task');
+const deleteTask = async (req, res) => {
+  try {
+    // find and delete single task documents
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(400).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // other response we can return,
+    // Since we are sending http 200 OK, means delete is complete.
+    // res.status(200).json({ task: null, status: 'success' });
+    // res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
