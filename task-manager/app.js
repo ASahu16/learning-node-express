91bde516499b6c5e5e66542f3ddf1dee24117bc4
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-03 23:01:38 +0530

    Update `static`
    - Use middleware `express.static`, update site
    - Remote old / path
    - Test working of api on the site

--- task-manager/app.js
+++ task-manager/app.js
@@ -7,13 +7,10 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 
 // middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
-  res.send('Welcome to Task Manager App');
-});
-
 app.use('/api/v1/tasks', tasks);
 
 const port = 5000;
