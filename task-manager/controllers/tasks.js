2b6647cac1fe3581a317a3c23fcc3c3d419f120a
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-03 16:39:06 +0530

    Update `deleteTask` controller
    Delete individual task details with matching `id` from the cloud

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -37,8 +37,22 @@ const updateTask = (req, res) => {
   res.send('update Task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete Task');
+const deleteTask = async (req, res) => {
+  try {
+    // find single task documents
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(400).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // other response we can return,
+    // Since we are sending http 200 OK, means delete is complete.
+    // res.status(200).json({ task: null, status: 'success' });
+    // res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
