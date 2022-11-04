b1fd758805d46b1012bd664168f55be835b23f3f
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-04 11:38:57 +0530

    Refactor `tasks` controller
    Wrap controller function in `asyncWrapper` middleware
    
    Remove `try-catch` from the controller methods

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,68 +1,49 @@
 const Task = require('../models/task');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-  try {
-    // find all documents
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  // find all documents
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-const getTask = async (req, res) => {
-  try {
-    // find single task documents
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
-    if (!task) {
-      return res.status(400).json({ msg: `No task with id : ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getTask = asyncWrapper(async (req, res) => {
+  // find single task documents
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    return res.status(400).json({ msg: `No task with id : ${taskID}` });
   }
-};
+  res.status(200).json({ task });
+});
 
-const updateTask = async (req, res) => {
-  try {
-    // update document
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const updateTask = asyncWrapper(async (req, res) => {
+  // update document
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ task });
+});
 
-const deleteTask = async (req, res) => {
-  try {
-    // find and delete single task documents
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-    if (!task) {
-      return res.status(400).json({ msg: `No task with id : ${taskID}` });
-    }
-    res.status(200).json({ task });
-    // other response we can return,
-    // Since we are sending http 200 OK, means delete is complete.
-    // res.status(200).json({ task: null, status: 'success' });
-    // res.status(200).send();
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const deleteTask = asyncWrapper(async (req, res) => {
+  // find and delete single task documents
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res.status(400).json({ msg: `No task with id : ${taskID}` });
   }
-};
+  res.status(200).json({ task });
+  // other response we can return,
+  // Since we are sending http 200 OK, means delete is complete.
+  // res.status(200).json({ task: null, status: 'success' });
+  // res.status(200).send();
+});
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
