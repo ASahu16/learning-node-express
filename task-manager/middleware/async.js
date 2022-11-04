cfe677d6cf46c3057d54cfb784fdd87a29056b78
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-04 11:37:12 +0530

    Add `asyncWrapper` middleware

---
+++ task-manager/middleware/async.js
@@ -0,0 +1,22 @@
+const asyncWrapper = (callback) => {
+  // Note: from express we get the request, response, next object
+  // which we pass on the the function we get as a callback.
+  //
+  // In simple words now we made code base more generic,
+  // by handling the error for the request as a middle functions
+  // request comes in the asyncWrapper function,
+  // then pass on the the controller function(passed as callback here)
+
+  // So, when a there is a error in the callback function it gets handled here,
+  // If you are familiar with java, its just like using the `throws` keywords
+  // outside the function
+  return async (req, res, next) => {
+    try {
+      await callback(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = asyncWrapper;
