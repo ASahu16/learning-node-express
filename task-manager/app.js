93fbca35251a920e0d10e9becc73bda8c2b3bc72
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-04 11:16:23 +0530

    Add middleware for invalid routes

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,10 +1,12 @@
+// load environment variables from a .env file into process.env
+require('dotenv').config();
+
+// import modules
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-
-// load environment variables from a .env file into process.env
-require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // middleware
 app.use(express.static('./public'));
@@ -12,6 +14,8 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
 
 const port = 5000;
 
